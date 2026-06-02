410ff829b6e8eb046ea87d7d944cd6c1d166cd33
전영신 <szylot67@gmail.com>
2026-06-02 09:06:58 +0900

    isolate-market-serverless-router

--- api/trpc/[...trpc].ts
+++ api/trpc/[...trpc].ts
@@ -1,7 +1,18 @@
 import { nodeHTTPRequestHandler } from "@trpc/server/adapters/node-http";
+import { initTRPC } from "@trpc/server";
 import type { IncomingMessage, ServerResponse } from "node:http";
-import { appRouter } from "../../server/routers";
-import { createContext } from "../../server/_core/context";
+import superjson from "superjson";
+import { getMarketSnapshot } from "../../server/marketData";
+
+const t = initTRPC.create({
+  transformer: superjson,
+});
+
+const serverlessRouter = t.router({
+  market: t.router({
+    snapshot: t.procedure.query(() => getMarketSnapshot()),
+  }),
+});
 
 function getTrpcPath(req: IncomingMessage) {
   const url = new URL(req.url ?? "", "http://localhost");
@@ -14,7 +25,7 @@ export default async function handler(req: IncomingMessage, res: ServerResponse)
     req,
     res,
     path: getTrpcPath(req),
-    router: appRouter,
-    createContext: async () => createContext({ req, res } as never),
+    router: serverlessRouter,
+    createContext: async () => ({}),
   });
 }
